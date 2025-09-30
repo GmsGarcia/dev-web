49cf731f4ee2569818f13ea678892376b6d04fde
GmsGarcia <gmsgarcia18@gmail.com>
2025-09-30 11:55:53 +0100

    lab3

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,10 +3,12 @@ window.addEventListener("scroll", () => {
   var header = document.getElementById("header");
   var scroll = document.documentElement.scrollTop;
 
+  // on page content
   if (scroll > hero.clientHeight - header.clientHeight) {
     header.style.backgroundColor = "#000";
   }
 
+  // on page hero
   if (scroll < hero.clientHeight - header.clientHeight) {
     header.style.backgroundColor = "#00000000";
   }
---
+++ lab3/assets/js/nav.js
@@ -0,0 +1,37 @@
+window.addEventListener("scroll", () => {
+  var hero = document.getElementById("hero");
+  var header = document.getElementById("header");
+  var links = document.querySelectorAll("#nav .links > a");
+  var scroll = document.documentElement.scrollTop;
+
+  // fix: a elements stay white after resizing
+  if (window.innerWidth < 700) {
+    links.forEach(a => {
+      a.style.color = "#000";
+    });
+  }
+
+  // on page content
+  if (scroll > hero.clientHeight - header.clientHeight) {
+    header.style.backgroundColor = "#FFF";
+    header.style.borderBottom = "2px solid #000";
+
+    if (window.innerWidth >= 700) {
+      links.forEach(a => {
+        a.style.color = "#000";
+      });
+    }
+  }
+
+  // on page hero
+  if (scroll < hero.clientHeight - header.clientHeight) {
+    header.style.backgroundColor = "#00000000";
+    header.style.borderBottom = "none";
+
+    if (window.innerWidth >= 700) {
+      links.forEach(a => {
+        a.style.color = "#FFF";
+      });
+    }
+  }
+})
