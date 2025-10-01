9f27b55da0e7881b610bac7d252e700a46b39516
GmsGarcia <gmsgarcia18@gmail.com>
2025-10-01 10:51:51 +0100

    :O

--- lab3/assets/js/nav.js
+++ lab3/assets/js/nav.js
@@ -2,6 +2,7 @@ window.addEventListener("scroll", () => {
   var hero = document.getElementById("hero");
   var header = document.getElementById("header");
   var logo = document.querySelector(".logo > a > img");
+  var bar = document.getElementById("bar");
   var links = document.querySelectorAll("#nav .links > a");
   var scroll = document.documentElement.scrollTop;
 
@@ -16,7 +17,8 @@ window.addEventListener("scroll", () => {
   if (scroll > hero.clientHeight - header.clientHeight) {
     header.style.backgroundColor = "#FFF";
     header.style.borderBottom = "2px solid #000";
-    logo.style.filter = "invert(100%)"
+    logo.style.filter = "invert(100%)";
+    bar.style.color = "#000";
 
     if (window.innerWidth >= 700) {
       links.forEach(a => {
@@ -30,6 +32,7 @@ window.addEventListener("scroll", () => {
     header.style.backgroundColor = "#00000000";
     header.style.borderBottom = "none";
     logo.style.filter = "invert(0%)"
+    bar.style.color = "#FFF";
 
     if (window.innerWidth >= 700) {
       links.forEach(a => {
