b47a378902dcf18a648a79b894f1a350147b4f4f
GmsGarcia <gmsgarcia18@gmail.com>
2025-10-22 18:42:37 +0100

    ...

--- lab5/assets/js/main.js
+++ lab5/assets/js/main.js
@@ -2,13 +2,29 @@
 var hoverEl = document.getElementById("hover");
 var colorEl = document.getElementById("color");
 var typerEl = document.getElementById("typer");
-var colorTextEl = document.getElementById("colorText");
+var selectEl = document.getElementById("select");
+
+var nameEl = document.getElementById("name");
+var ageEl = document.getElementById("age");
+var labelEl = document.getElementById("label");
+
 var counterEl = document.getElementById("counter");
+var autoEl = document.getElementById("auto");
+
+var colors = ["#CEF7A0", "#D7A6B3", "#AEECEF", "#FFE19C"];
 
 var c = 0;
+var ac = 0;
+
 
 window.addEventListener("load", () => {
   counterEl.innerText = c;
+  autoEl.innerText = ac;
+
+  setInterval(() => {
+    ac++;
+    autoEl.innerText = ac;
+  }, 1000)
 
 })
 
@@ -20,29 +36,27 @@ hoverEl.addEventListener("mouseleave", () => {
   hoverEl.innerText = "Passa por aqui!";
 })
 
-typerEl.addEventListener("input", () => {
-  var random = Math.floor(Math.random()*16777215).toString(16);
-  typerEl.style.backgroundColor = `#${random}`;
-}) 
-
-function count() {
-  c++;
-  counterEl.innerText = c;
-}
-
+// rgb buttons
 function color(color) {
   colorEl.style.color = color;
 }
 
-function colorText() {
-  document.body.style.backgroundColor = colorTextEl.value;
+typerEl.addEventListener("input", () => {
+  var rn = Math.floor(Math.random() * 4);
+  typerEl.style.backgroundColor = colors[rn];
+})
+
+selectEl.onchange = () => {
+  document.body.style.backgroundColor = selectEl.value;
 }
 
+// label
+function label() {
+  labelEl.innerText = `Olá, o ${nameEl.value} tem ${ageEl.value}!`;
+}
+
+// counter
 function count() {
   c++;
   counterEl.innerText = c;
-}
-
-function type(color) {
-  colorEl.style.color = color;
 }
---
+++ lab6/main.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", () => {
+  var container = document.getElementById("produtos");
+})
+
